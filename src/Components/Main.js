26ae263d4fe727cbfb99d4b3057ebafd78000364
Moustapha76 <moustapha.md26@gmail.com>
2024-08-22 09:35:36 +0100

    about

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -37,7 +37,7 @@ export default function Main() {
   // };
   useEffect(() => {
     setStore(products.map((item) => item));
-  }, []);
+  }, [products]);
   return (
     <div className="main">
       <input
@@ -59,7 +59,7 @@ export default function Main() {
             <span className="price">{el.price} €</span>
             {/* {el.addCart === false && <button onClick={() => addToCart(el)} className="addCart btn">Ajouter à la carte <i class="fa-solid fa-cart-plus"></i></button>}     */}
             {/* {el.addCart && <button onClick={() => removeFromCart(el)} className="remove btn">Retirer de la carte <i class="fa-solid fa-cart-shopping"></i></button> }   */}
-            <a role="button" href="" onClick={()=>goToArticle(el.id)} className="btn">Voir l'article <i class="fa-solid fa-eye-open"></i></a>  
+            <button onClick={()=>goToArticle(el.id)} className="btn">Voir l'article <i class="fa-solid fa-eye-open"></i></button>  
           </div>
         </div>
         );
@@ -80,13 +80,29 @@ export default function Main() {
               <span className="price">{el.price} €</span>
               {/* {el.addCart === false && <a role="button" href="#" onClick={() => addToCart(el)} className="addCart btn">Ajouter à la carte <i class="fa-solid fa-cart-plus"></i></a>}    
               {el.addCart && <a role="button" href="#" onClick={() => removeFromCart(el)} className="remove btn">Retirer de la carte <i class="fa-solid fa-cart-shopping"></i></a> }   */}
-              <a role="button" href="" onClick={()=>goToArticle(el.id)} className="btn">Voir l'article <i class="fa-solid fa-eye-open"></i></a>  
+              <button onClick={()=>goToArticle(el.id)} className="btn">Voir l'article <i class="fa-solid fa-eye-open"></i></button>  
             </div>
           </div>
           );
         })}
         </div>
         {filteredList.length === 0 && noFound && <h2>Aucun résultat trouvé</h2>}
+      </section>
+      <section className="about">
+        <h2>Luxe Mocassin<span className="subtitle">Qui sommes-nous ?</span></h2>
+        <div className="container">
+          <div className="textcontent">
+            <p><strong>Luxe Mocassin</strong> est une boutique spécialisée dans la vente de mocassins de luxe à Casablanca.</p>
+            <p>Nous proposons des chaussures raffinées, confectionnées avec des matériaux haut de gamme pour un confort et une élégance inégalés.</p> 
+            <p>Chaque paire est conçue pour sublimer votre style avec une touche d'exclusivité, que ce soit pour des occasions formelles ou un usage quotidien.</p> 
+            <p>Découvrez l’alliance parfaite entre tradition artisanale et modernité chez Luxe Mocassin, où chaque détail compte pour offrir une expérience unique à nos clients.</p>
+          </div>
+          <div className="img-content">
+            <img src="../images/mocassin-noir.jpg" alt="Mocassin"/>
+            <img src="../images/elegant-mocassin.jpg" alt="Mocassin"/>
+            <img src="../images/luxe-mocassin.jpg" alt="Mocassin"/>
+          </div>
+        </div>
       </section>
     </div>
   );
