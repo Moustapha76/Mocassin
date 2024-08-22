6112b2b679ef3c3bd3c3ace6f80e5a3860e7f493
Moustapha76 <moustapha.md26@gmail.com>
2024-08-22 12:30:43 +0100

    marques

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
         <div className="banner">
           <h1>Bienvenue chez Luxe Mocassins</h1>
           <span>Découvrez notre collection de mocassins en cuir 100% authentique</span>
-          <a role="button" className="shop-button">Découvrez nos produits</a>
+          <a href="/article" role="button" className="shop-button">Découvrez nos produits</a>
         </div>
       </header>
     );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -47,6 +47,12 @@ export default function Main() {
         id="search-box"
         onChange={filterBySearch}
       />
+        <section className="atouts">
+          <div className="item-atout"><i class="fa-solid fa-truck"></i><span>Livraison express</span><p>Service de livraison disponile 24h/24</p></div>
+          <div className="item-atout"><i class="fa-solid fa-award"></i><span>100% Garantie</span><p>Nos mocassins sont garanties pour 24 mois</p></div>
+          <div className="item-atout"><i class="fa-solid fa-headset"></i><span>Service client</span><p>Service client joignable en ligne 24h/24</p></div>
+          <div className="item-atout"><i class="fa-solid fa-percent"></i><span>Promotion</span><p>Avec Luxe Mocassins, obtenez le luxe à bas pris</p></div>
+        </section>
         <section className="products">
         {filteredList.map((el, k) => {
         return (
@@ -59,18 +65,17 @@ export default function Main() {
             <span className="price">{el.price} €</span>
             {/* {el.addCart === false && <button onClick={() => addToCart(el)} className="addCart btn">Ajouter à la carte <i class="fa-solid fa-cart-plus"></i></button>}     */}
             {/* {el.addCart && <button onClick={() => removeFromCart(el)} className="remove btn">Retirer de la carte <i class="fa-solid fa-cart-shopping"></i></button> }   */}
-            <button onClick={()=>goToArticle(el.id)} className="btn">Voir l'article <i class="fa-solid fa-eye-open"></i></button>  
+            <button onClick={()=>goToArticle(el.id)}>Voir l'article <i class="fa-solid fa-eye-open"></i></button>  
           </div>
         </div>
         );
         })}
-        <h2>Nos Mocassins en Vedette</h2>
+        <h2>Nos Mocassins en Vedette<span className="subtitle">Nos best sellers</span></h2>
         <div className="product-container">
         {filteredList.length === 0 &&
         !noFound &&
         store.map((el, k) => {
           return (
-            // <Card key={k} name={el.name} price={el.price} imageSrc={el.image}>
           <div className="card" key={k}>
             <div className="img-content">
               <img src={el.image} alt={el.image} />
@@ -80,7 +85,7 @@ export default function Main() {
               <span className="price">{el.price} €</span>
               {/* {el.addCart === false && <a role="button" href="#" onClick={() => addToCart(el)} className="addCart btn">Ajouter à la carte <i class="fa-solid fa-cart-plus"></i></a>}    
               {el.addCart && <a role="button" href="#" onClick={() => removeFromCart(el)} className="remove btn">Retirer de la carte <i class="fa-solid fa-cart-shopping"></i></a> }   */}
-              <button onClick={()=>goToArticle(el.id)} className="btn">Voir l'article <i class="fa-solid fa-eye-open"></i></button>  
+              <button onClick={()=>goToArticle(el.id)}>Voir l'article <i class="fa-solid fa-eye-open"></i></button>  
             </div>
           </div>
           );
@@ -88,20 +93,44 @@ export default function Main() {
         </div>
         {filteredList.length === 0 && noFound && <h2>Aucun résultat trouvé</h2>}
       </section>
+      <section className="promo">
+        <h2>Super promos</h2>
+        <p>Promotion du 30 Août au 10 Septembre 2024 ! Réduction de <strong>50%</strong> sur tout nos produits.</p>
+      </section>
       <section className="about">
-        <h2>Luxe Mocassin<span className="subtitle">Qui sommes-nous ?</span></h2>
         <div className="container">
           <div className="textcontent">
-            <p><strong>Luxe Mocassin</strong> est une boutique spécialisée dans la vente de mocassins de luxe à Casablanca.</p>
-            <p>Nous proposons des chaussures raffinées, confectionnées avec des matériaux haut de gamme pour un confort et une élégance inégalés.</p> 
-            <p>Chaque paire est conçue pour sublimer votre style avec une touche d'exclusivité, que ce soit pour des occasions formelles ou un usage quotidien.</p> 
-            <p>Découvrez l’alliance parfaite entre tradition artisanale et modernité chez Luxe Mocassin, où chaque détail compte pour offrir une expérience unique à nos clients.</p>
+            <h2>Luxe Mocassin<span className="subtitle">Qui sommes-nous ?</span></h2>
+            <div>
+              <p><strong>Luxe Mocassin</strong> est une boutique spécialisée dans la vente de mocassins de luxe à Casablanca.</p>
+              <p>Nous proposons des chaussures raffinées, confectionnées avec des matériaux haut de gamme pour un confort et une élégance inégalés.</p> 
+              <p>Chaque paire est conçue pour sublimer votre style avec une touche d'exclusivité, que ce soit pour des occasions formelles ou un usage quotidien.</p> 
+              <p>Découvrez l’alliance parfaite entre tradition artisanale et modernité chez Luxe Mocassin, où chaque détail compte pour offrir une expérience unique à nos clients.</p>
+            </div>
+            <div className="btn-group">
+              <a href="/contact" className="btn">Contactez-nous <i class="fa-solid fa-phone-flip"></i></a>
+              <a href="/article" className="btn">Visitez la boutique <i class="fa-solid fa-phone-flip"></i></a>
+            </div>
           </div>
           <div className="img-content">
-            <img src="../images/mocassin-noir.jpg" alt="Mocassin"/>
-            <img src="../images/elegant-mocassin.jpg" alt="Mocassin"/>
-            <img src="../images/luxe-mocassin.jpg" alt="Mocassin"/>
+            {/* <div><img src="../images/17.jpg" alt="Mocassin"/></div>
+            <div><img src="../images/20.jpg" alt="Mocassin"/></div> */}
+            <div><img src="../images/13.jpg" alt="Mocassin"/></div>
+            <div><img src="../images/modern-mocassin.jpg" alt="Mocassin"/></div>
+            {/* <div><img src="../images/comfort-mocassin.jpg" alt="Mocassin"/></div> */}
           </div>
+        </div>
+      </section>
+      <section className="marques">
+        <div className="logo-container">
+          <div><img src="../images/marques/alden-shoe-company-seeklogo.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/carmina-seeklogo.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/churchs.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/gucci-seeklogo-4.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/john-lobb-seeklogo.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/salvatore-ferragamo-s-p-a-seeklogo.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/Santoni.svg" alt="Marque"/></div>
+          <div><img src="../images/marques/tods-seeklogo.svg" alt="Marque"/></div>
         </div>
       </section>
     </div>
